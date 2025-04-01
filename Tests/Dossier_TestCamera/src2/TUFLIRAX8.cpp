582f9229ead9c5bab25d38c28aa6e8d0bf90cf38
NAghopourVersigny <naghopourversigny@btslivh.eu>
2025-04-01 08:41:34 +0200

    Modification fichier TUFLIRAX8.cpp : Utilisation méthodes Connexion_Modbus et Deconnexion_Modbus

--- Tests/Dossier_TestCamera/src2/TUFLIRAX8.cpp
+++ Tests/Dossier_TestCamera/src2/TUFLIRAX8.cpp
@@ -6,8 +6,20 @@ int main(){
     //Instantiation des objets 
     FLIR_AX8* laFLIR_AX8 = new FLIR_AX8("neco-10655D.local");
 
-    //Lancement de la méthode pour établir une connexion Modbus TCP
-    laFLIR_AX8-> ConnectionFLIR_AX8();
+    //Lancement de la méthode pour établir une connexion Modbus TCP ou une déconnexion
+    cout << "Choisir une option" << endl;
+    cout << "Connexion : 1" << endl;
+    cout << "Déconnexion : 2" << endl;
 
+    int choix;
+    cin >> choix;
+    if (choix ==1){
+    laFLIR_AX8-> Connection_Modbus();
+    cout << "Connexion établie" << endl;
+    } else if (choix ==2)
+    {
+    laFLIR_AX8-> Deconnection_Modbus();
+    cout << "Déconnexion" << endl;
+    }
     return 0;
 }
