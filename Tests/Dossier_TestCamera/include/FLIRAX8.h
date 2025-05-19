5322417b64865f6e1b6b2f3ad3988dc965c858cf
NAghopourVersigny <naghopourversigny@btslivh.eu>
2025-05-19 10:49:07 +0200

    Ajout fichiers de test de faisabilité Camera protocole Modbus

--- Tests/Dossier_TestCamera/include/FLIRAX8.h
+++ Tests/Dossier_TestCamera/include/FLIRAX8.h
@@ -13,6 +13,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <cmath>
+#include <curl/curl.h>
 
 using namespace std;
 
@@ -54,11 +55,11 @@ private:
     const char *username = "fliruser";
     const char *password = "3vlig";
 
-	string resolveHostname(const std::string &hostname);
-
+	string resolveHostname(const string &hostname);
 
 public:
 	FLIR_AX8(string hostname);
+void set_image_mode(int mode);
 
 	int ConnectionFLIR_AX8();
 	void Connection_Modbus();
@@ -69,16 +70,22 @@ public:
 	float acquerirTempMoy();
 	float acquerirTempMax();
 	float acquerirTempMin();
+	int setVisionMode(int mode);
+	CURLcode perform_request(const string& url, const string& username, const string& password, string& response, bool isPost = false, const string& postData = "");
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output);
+	string get_image_mode();
+	void set_image_mode_IR();
+	void set_image_mode_VISUAL();
 	int mettre_visible();
-	//int mettre_thermique();
+
  
 	int supprimer_image();
-
 
 	//void obtenirEvent();
 
 	void store_Image();
 	bool alarme();
 };
+
 
 #endif
