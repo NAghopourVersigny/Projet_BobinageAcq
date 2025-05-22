0dff575182fe6c1e1977a6dab048b5590787a729
rfonteneau <rfonteneau@btslivh.eu>
2025-05-22 16:22:26 +0200

    Ajout Test Integration Ambiance

---
+++ Tests/Dossier_TestCapteur/TIAmbiance/TIAmbiance.cpp
@@ -0,0 +1,16 @@
+#include "../include/Ambiance.h"
+#include "../include/TempHumi.h"
+#include <iostream>
+#include <linux/i2c-dev.h>
+#include <fcntl.h>
+#include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
