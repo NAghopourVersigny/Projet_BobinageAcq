1af42dcbf035ecabab53e3895086bc9c4d0add7f
NAghopourVersigny <naghopourversigny@btslivh.eu>
2025-05-26 14:01:58 +0200

    Mise à jour script SeuilsIOT.h : Ajout des attributs et méthodes restantes

--- Tests/Dossier_TestCamera/include/SeuilsIOT.h
+++ Tests/Dossier_TestCamera/include/SeuilsIOT.h
@@ -6,6 +6,9 @@ class SeuilsIOT
 {
 private:
     float seuilTempMax, seuilTempMoy;
+    float AlerteVibX, AlerteVibY, AlerteVibZ;
+    float AlarmeVibX, AlarmeVibY, AlarmeVibZ;
+    
 public:
     SeuilsIOT();
     ~SeuilsIOT();
@@ -16,12 +19,28 @@ public:
      * @param valMax 
      */
     void setSeuils_temperature(float valMoy, float valMax);
+    float getSeuilTempMax();
     float getTempMax();
+    void setSeuilTempMax(float seuilTempMax);
+    float getSeuilTempMoy();
+    void setSeuilTempMoy(float seuilTempMoy);
     float getTempMoy();
     
 
+
+
     void setSeuils_vibrations(float seuilAxeX, float seuilAxeY, float seuilAxeZ);
-    float getTempMax();
-    float getTempMoy();
+    float getSeuilAlertVibX();
+    void setSeuilAlerteVibX(float seuilAlerteVibX);
+    float getSeuilAlarmeVibX();
+    void setSeuilAlarmeVibX(float seuilAlarmeVibX);
+    float getSeuilAlertVibY();
+    void setSeuilAlerteVibY(float seuilAlerteVibY);
+    float getSeuilAlarmeVibY();
+    void setSeuilAlarmeVibY(float seuilAlarmeVibY);
+    float getSeuilAlertVibZ();
+    void setSeuilAlerteVibZ(float seuilAlerteVibZ);
+    float getSeuilAlarmeVibZ();
+    void setSeuilAlarmeVibZ(float seuilAlarmeVibZ);
 };
 #endif
