e99fad52d134d4dcbb7ab3787604b2c12ab778f0
rfonteneau <rfonteneau@btslivh.eu>
2025-05-27 11:04:37 +0200

    Mise a jour Ambiance.cpp Test Integration : Remplissage methode EnregistrerAmbiance

--- Tests/Dossier_TestCapteur/TIAmbiance/src/Ambiance.cpp
+++ Tests/Dossier_TestCapteur/TIAmbiance/src/Ambiance.cpp
@@ -1,5 +1,6 @@
 #include "../include/Ambiance.h"
 #include "../include/TempHumi.h"
+#include "../include/bdd.h"
 #include <iostream>
 #include <linux/i2c-dev.h>
 #include <fcntl.h>
@@ -8,6 +9,8 @@
 #include <unistd.h>
 
 using namespace std;
+using namespace sql;
+using namespace mariadb;
 
 // Constructeur de la classe Ambiance. Il initialise l'I2C du capteur.
 Ambiance::Ambiance(int addr)
@@ -20,7 +23,18 @@ int Ambiance::AcquérirAmbiance() {
 }
 
 int Ambiance::EnregistrerAmbiance() {
-    return 0;
+        BDD *laBDD = new BDD();
+    unique_ptr<Connection> conn = laBDD->SeConnecterBDD();
+    cout << "Préparation de la requête" << endl;
+
+    unique_ptr<PreparedStatement> stmnt(conn->prepareStatement("INSERT INTO Contexte (datetime, tempAmb, humidite) VALUES(?,?,?)"));
+    stmnt->setInt(1, 1);
+
+    cout << "Exécution de la requête" << endl;
+    stmnt->execute();
+
+    return true;
+    
 }
 
 // Méthode qui retourne la valeur de la température
