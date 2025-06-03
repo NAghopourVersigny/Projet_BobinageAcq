94903f988c37cc93b09f4ef5d971254f93b5d19f
rfonteneau <rfonteneau@btslivh.eu>
2025-06-03 12:02:52 +0200

    Modification fichier MQTT : declarations

--- include/MQTT.h
+++ include/MQTT.h
@@ -1,5 +1,5 @@
-#ifndef MQTT_CLIENT_H
-#define MQTT_CLIENT_H
+#ifndef MQTTCLIENT_H
+#define MQTTCLIENT_H
 
 #include <string>
 #include <mosquitto.h>
@@ -11,39 +11,61 @@ public:
 
     bool connect();
     void disconnect();
-    bool publishTemperature();
     bool subscribeAndListen();
 
-    void onMessageTmax(const std::string& tmax);
-    void onMessageTmoy(const std::string& tmoy);
-    void onMessageDateTime(const std::string& dt);
-    void onMessageHumidite(const std::string& humidite);
-    void onMessageTemperature(const std::string& temperature);
-    void onMessageVibration(const std::string& vibration);
+    bool publishTemperatureMachine(int machineId);
+    bool publishVibrations(int machineId);
+    bool publishMACCamera(int machineId, const std::string& macAdresse);
+    bool publishApprentissageSeuilsTemp(int machineId);
+    bool publishApprentissageSeuilsVibration(int machineId);
+    bool publishSeuils(int machineId);
+
+    void ReceptionseuilsMessage(const std::string& payload);
+    void ReceptionTempMachineMessage(const std::string& payload);
+    void ReceptionVibrationsMessage(const std::string& payload);
+    void ReceptionApprentissageTempMessage(const std::string& payload);
+    void ReceptionApprentissageVibrationMessage(const std::string& payload);
 
     double getTemperatureMax();
     double getTemperatureMoy();
-    double getHumidite();
-    double getTemperature();
-    double getVibration();
+    double getVibrationX();
+    double getVibrationY();
+    double getVibrationZ();
     std::string getCurrentDateTime();
 
-    void afficherDonnees();
-    
 private:
+   int machineId;
     std::string host_;
     int port_;
     std::string topic_;
     struct mosquitto* mosq_;
 
-    static void onMessage(struct mosquitto* mosq, void* obj, const struct mosquitto_message* message);
-
-    std::string tmax_;
-    std::string tmoy_;
+    // Données capteurs
     std::string dateTime_;
-    std::string humidite_;
-    std::string temperature_;
-    std::string vibration_;
+    std::string tmax_, tmoy_;
+    double vibrationX_ = 0;
+    double vibrationY_ = 0;
+    double vibrationZ_ = 0;
+
+    // Seuils dynamiques
+    double seuil_temp_moy_alerte_ = 0;
+    double seuil_temp_max_alerte_ = 0;
+    double seuil_temp_moy_alarme_ = 0;
+    double seuil_temp_max_alarme_ = 0;
+    double seuil_vibration_alerte_ = 0;
+    double seuil_vibration_alarme_ = 0;
+    bool seuilsInitialises_ = false;
+
+    // Apprentissage
+    bool apprentissage_temperature_ = false;
+    bool apprentissage_vibration_ = false;
+    int apprentissage_duree_temp_ = 0;
+    int apprentissage_duree_vib_ = 0;
+    double coeff_temp_ = 1.0;
+    double coeff_vib_alerte_ = 1.0;
+    double coeff_vib_alarme_ = 1.0;
 };
 
-#endif // MQTT_CLIENT_H
+#endif
+
+
