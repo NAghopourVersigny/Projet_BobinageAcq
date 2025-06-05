c964e7f65bcabbd236fce319dea7e4faa6137b1f
rfonteneau <rfonteneau@btslivh.eu>
2025-06-05 14:28:27 +0200

    Mise a jour Script DatabaseManager.cpp

--- Tests/Dossier_TestCapteur/TIAmbiance/src/DatabaseManager.cpp
+++ Tests/Dossier_TestCapteur/TIAmbiance/src/DatabaseManager.cpp
@@ -1,42 +1,49 @@
 #include <iostream>
 #include <mariadb/conncpp.hpp>
-#include "../include/bdd.h"
-
+#include "../include/DatabaseManager.h"
 
 using namespace std;
 using namespace sql;
 using namespace mariadb;
 
-
-BDD::BDD()
+DatabaseManager::DatabaseManager()
 {
-   this->url = SQLString("jdbc:mariadb://localhost:3306/Bobinage");
+    this->url = SQLString("jdbc:mariadb://localhost:3306/Bobinage");
 }
 
+string DatabaseManager::executerRequete(string StringQuery){
 
-SQLString BDD::getURL()
-{
-   return this->url;
+    SQLString conversion(StringQuery);
+    DatabaseManager *leDatabaseManager = new DatabaseManager();
+
+    unique_ptr<Connection> conn = leDatabaseManager->SeConnecterBDD();
+
+    unique_ptr<PreparedStatement> stmnt(conn->prepareStatement(conversion));
+    stmnt->setInt(1, 1);
+
+    stmnt->execute();
+
+    return 0;
 }
 
-
-unique_ptr<Connection> BDD::SeConnecterBDD()
+SQLString DatabaseManager::getURL()
 {
 
-   // Instantiate Driver
-   Driver *driver = get_driver_instance();
-
-
-   // Configure Connection
-   Properties properties({{"user", "adminECEI"}, {"password", "adminECEI"}});
-
-
-   // Establish Connection
-   unique_ptr<Connection> conn(driver->connect(url, properties));
-
-
-   cout << "Connexion réussie" << endl;
-
-
-   return conn;
+    return this->url;
 }
+
+unique_ptr<Connection> DatabaseManager::SeConnecterBDD(){
+
+    // Instantiate Driver
+    Driver *driver = get_driver_instance();
+
+    // Configure Connection
+    Properties properties({{"user", "adminECEI"}, {"password", "adminECEI"}});
+
+    // Establish Connection
+    unique_ptr<Connection> conn(driver->connect(url, properties));
+
+    cout << "Connexion réussie" << endl;
+
+    return conn;
+ }
