6358f72d408511a841bc0d0617d91909ddac7373
NAghopourVersigny <naghopourversigny@btslivh.eu>
2025-06-05 14:32:14 +0200

    Mise à jour fichier DatabaseManager.cpp (Dossier_TestCamera)

--- Tests/Dossier_TestCamera/src2/DatabaseManager.cpp
+++ Tests/Dossier_TestCamera/src2/DatabaseManager.cpp
@@ -1,18 +1,49 @@
 #include <iostream>
-#include "../../../mariadb-connector-cpp-1.0.2-rhel9-amd64/include/mariadb/conncpp.hpp"
-#include "../include/bdd.h"
+#include <mariadb/conncpp.hpp>
+#include "../include/DatabaseManager.h"
 
 using namespace std;
 using namespace sql;
 using namespace mariadb;
 
-BDD::BDD(SQLString url){
-
-this->url = url;
+DatabaseManager::DatabaseManager()
+{
+    this->url = SQLString("jdbc:mariadb://localhost:3306/Bobinage");
 }
 
-SQLString BDD::getURL(){
+string DatabaseManager::executerRequete(string StringQuery){
 
-return this->url;
+    SQLString conversion(StringQuery);
+    DatabaseManager *leDatabaseManager = new DatabaseManager();
 
+    unique_ptr<Connection> conn = leDatabaseManager->SeConnecterBDD();
+
+    unique_ptr<PreparedStatement> stmnt(conn->prepareStatement(conversion));
+    stmnt->setInt(1, 1);
+
+    stmnt->execute();
+
+    return 0;
 }
+
+SQLString DatabaseManager::getURL()
+{
+
+    return this->url;
+}
+
+unique_ptr<Connection> DatabaseManager::SeConnecterBDD(){
+
+    // Instantiate Driver
+    Driver *driver = get_driver_instance();
+
+    // Configure Connection
+    Properties properties({{"user", "adminECEI"}, {"password", "adminECEI"}});
+
+    // Establish Connection
+    unique_ptr<Connection> conn(driver->connect(url, properties));
+
+    cout << "Connexion réussie" << endl;
+
+    return conn;
+ }
