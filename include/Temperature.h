8684291832bb36d35557a556929d331e1ade335e
mbollerot <mbollerot@btslivh.eu>
2025-06-12 12:03:33 +0200

    ajout de temperature.h

--- include/Temperature.h
+++ include/Temperature.h
@@ -8,13 +8,19 @@ using namespace std;
 class Temperature
 {
 private:
-    float valeur;
+    float moyenne;
+    float maximale;
     string dateheure;
 
 public:
-    Temperature(float valeur, string dateheure);
-    float getValeur();
+    Temperature();
+    Temperature(float moyenne, float maximale, string dateheure);
+    float getMoyenne();
+    float getMaximale();
     string getDateHeure();
+    void setMoyenne(float moyenne);
+    void setMaximale(float maximale);
+    void setDateHeure(string dateheure);
 };
 
 #endif
