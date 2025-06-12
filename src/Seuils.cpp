3f925658a803232989aa4f83961efc7e249ffe6c
mbollerot <mbollerot@btslivh.eu>
2025-06-12 11:56:56 +0200

    ajout de seuils.cpp

--- src/Seuils.cpp
+++ src/Seuils.cpp
@@ -1,31 +1,76 @@
 #include "../include/Seuils.h"
+#include <iostream>
 
 Seuils::Seuils()
 {
-    this->base=new Base();
-    this->seuilTempMax=0;
-    this->seuilTempMoy=0;
-
+    this->seuilTempMax;
+    this->seuilTempMoy;
 }
 
-void Seuils::setSeuils_temperature(float valMoy, float valMax, int idMachine)
-{
-    this->seuilTempMax=valMax;
-    this->seuilTempMoy=valMoy; 
-    base->ajouterSeuilTemp(seuilTempMax,seuilTempMoy,1); 
+Seuils::~Seuils() {}
+
+void Seuils::setSeuils_temperature(float valMoy, float valMax, int idMachine) {
+    seuilTempMoy = valMoy;
+    seuilTempMax = valMax;
 }
 
-float Seuils::getTempMax()
-{
+float Seuils::getTempMax() {
     return seuilTempMax;
 }
 
-float Seuils::getTempMoy()
-{
+float Seuils::getTempMoy() {
     return seuilTempMoy;
 }
 
-Seuils::~Seuils()
+float Seuils::getSeuilAlerteVibX() {
+    return seuilAlerteVibX;
+}
+
+void Seuils::setSeuilAlerteVibX(float seuilAlerteVibX) {
+    this->seuilAlerteVibX = seuilAlerteVibX;
+}
+
+float Seuils::getSeuilAlarmeVibX() {
+    return seuilAlarmeVibX;
+}
+
+void Seuils::setSeuilAlarmeVibX(float seuilAlarmeVibX) {
+    this->seuilAlarmeVibX = seuilAlarmeVibX;
+}
+
+float Seuils::getSeuilAlerteVibY() {
+    return seuilAlerteVibY;
+}
+
+void Seuils::setSeuilAlerteVibY(float seuilAlerteVibY) {
+    this->seuilAlerteVibY = seuilAlerteVibY;
+}
+
+float Seuils::getSeuilAlarmeVibY() {
+    return seuilAlarmeVibY;
+}
+
+void Seuils::setSeuilAlarmeVibY(float seuilAlarmeVibY) {
+    this->seuilAlarmeVibY = seuilAlarmeVibY;
+}
+
+float Seuils::getSeuilAlerteVibZ() {
+    return seuilAlerteVibZ;
+}
+
+void Seuils::setSeuilAlerteVibZ(float seuilAlerteVibZ) {
+    this->seuilAlerteVibZ = seuilAlerteVibZ;
+}
+
+float Seuils::getSeuilAlarmeVibZ() {
+    return seuilAlarmeVibZ;
+}
+
+void Seuils::setSeuilAlarmeVibZ(float seuilAlarmeVibZ) {
+    this->seuilAlarmeVibZ = seuilAlarmeVibZ;
+}
+
+void Seuils::enregistrer(DatabaseManager *db)
 {
-    //destructeur
+    this->enregistrer(db);
 }
